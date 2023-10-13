7bfc45a2b250c9b8b2cb54cd4b316d18af127298
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-10-13 13:25:30 +0000

    Release 0.1.10

--- src/main/java/com/vital/api/core/ClientOptions.java
+++ src/main/java/com/vital/api/core/ClientOptions.java
@@ -23,7 +23,12 @@ public final class ClientOptions {
         this.headers = new HashMap<>();
         this.headers.putAll(headers);
         this.headers.putAll(Map.of(
-                "X-Fern-SDK-Name", "com.vital.fern:api-sdk", "X-Fern-SDK-Version", "0.1.9", "X-Fern-Language", "JAVA"));
+                "X-Fern-SDK-Name",
+                "com.vital.fern:api-sdk",
+                "X-Fern-SDK-Version",
+                "0.1.10",
+                "X-Fern-Language",
+                "JAVA"));
         this.headerSuppliers = headerSuppliers;
         this.httpClient = httpClient;
         ;
