a80437dfce3cd315f23d9473ecdb9da051169cec
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-04-19 00:20:13 +0000

    Release 1.2.268

--- src/main/java/com/vital/api/types/ClientFacingPatientDetailsCompatible.java
+++ src/main/java/com/vital/api/types/ClientFacingPatientDetailsCompatible.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.annotation.Nulls;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.vital.api.core.ObjectMappers;
+import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -24,7 +25,7 @@ public final class ClientFacingPatientDetailsCompatible {
 
     private final Optional<String> lastName;
 
-    private final String dob;
+    private final OffsetDateTime dob;
 
     private final String gender;
 
@@ -47,7 +48,7 @@ public final class ClientFacingPatientDetailsCompatible {
     private ClientFacingPatientDetailsCompatible(
             Optional<String> firstName,
             Optional<String> lastName,
-            String dob,
+            OffsetDateTime dob,
             String gender,
             Optional<String> phoneNumber,
             Optional<String> email,
@@ -82,7 +83,7 @@ public final class ClientFacingPatientDetailsCompatible {
     }
 
     @JsonProperty("dob")
-    public String getDob() {
+    public OffsetDateTime getDob() {
         return dob;
     }
 
@@ -193,7 +194,7 @@ public final class ClientFacingPatientDetailsCompatible {
     }
 
     public interface DobStage {
-        GenderStage dob(String dob);
+        GenderStage dob(OffsetDateTime dob);
 
         Builder from(ClientFacingPatientDetailsCompatible other);
     }
@@ -244,7 +245,7 @@ public final class ClientFacingPatientDetailsCompatible {
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static final class Builder implements DobStage, GenderStage, _FinalStage {
-        private String dob;
+        private OffsetDateTime dob;
 
         private String gender;
 
@@ -289,7 +290,7 @@ public final class ClientFacingPatientDetailsCompatible {
 
         @java.lang.Override
         @JsonSetter("dob")
-        public GenderStage dob(String dob) {
+        public GenderStage dob(OffsetDateTime dob) {
             this.dob = dob;
             return this;
         }
