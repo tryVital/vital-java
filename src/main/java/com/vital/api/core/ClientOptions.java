e53aba3a6ebf1c04a5c364d010f73874fc0962c5
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-05-20 19:29:11 +0000

    Release 1.2.286

--- src/main/java/com/vital/api/core/ClientOptions.java
+++ src/main/java/com/vital/api/core/ClientOptions.java
@@ -30,7 +30,7 @@ public final class ClientOptions {
             {
                 put("X-Fern-Language", "JAVA");
                 put("X-Fern-SDK-Name", "com.vital.fern:api-sdk");
-                put("X-Fern-SDK-Version", "1.2.285");
+                put("X-Fern-SDK-Version", "1.2.286");
             }
         });
         this.headerSuppliers = headerSuppliers;
