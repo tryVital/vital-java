87d646ebf2870b937d51eada4430a2ece92fdbec
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-30 11:42:06 +0000

    Release 1.2.424

--- src/main/java/com/vital/api/Vital.java
+++ src/main/java/com/vital/api/Vital.java
@@ -32,13 +32,11 @@ import java.util.function.Supplier;
 public class Vital {
     protected final ClientOptions clientOptions;
 
+    protected final Supplier<ProvidersClient> providersClient;
+
     protected final Supplier<UserClient> userClient;
 
     protected final Supplier<LinkClient> linkClient;
-
-    protected final Supplier<ProvidersClient> providersClient;
-
-    protected final Supplier<TeamClient> teamClient;
 
     protected final Supplier<ElectrocardiogramClient> electrocardiogramClient;
 
@@ -62,6 +60,8 @@ public class Vital {
 
     protected final Supplier<VitalsClient> vitalsClient;
 
+    protected final Supplier<TeamClient> teamClient;
+
     protected final Supplier<IntrospectClient> introspectClient;
 
     protected final Supplier<LabTestsClient> labTestsClient;
@@ -78,10 +78,9 @@ public class Vital {
 
     public Vital(ClientOptions clientOptions) {
         this.clientOptions = clientOptions;
+        this.providersClient = Suppliers.memoize(() -> new ProvidersClient(clientOptions));
         this.userClient = Suppliers.memoize(() -> new UserClient(clientOptions));
         this.linkClient = Suppliers.memoize(() -> new LinkClient(clientOptions));
-        this.providersClient = Suppliers.memoize(() -> new ProvidersClient(clientOptions));
-        this.teamClient = Suppliers.memoize(() -> new TeamClient(clientOptions));
         this.electrocardiogramClient = Suppliers.memoize(() -> new ElectrocardiogramClient(clientOptions));
         this.sleepCycleClient = Suppliers.memoize(() -> new SleepCycleClient(clientOptions));
         this.profileClient = Suppliers.memoize(() -> new ProfileClient(clientOptions));
@@ -93,6 +92,7 @@ public class Vital {
         this.mealClient = Suppliers.memoize(() -> new MealClient(clientOptions));
         this.menstrualCycleClient = Suppliers.memoize(() -> new MenstrualCycleClient(clientOptions));
         this.vitalsClient = Suppliers.memoize(() -> new VitalsClient(clientOptions));
+        this.teamClient = Suppliers.memoize(() -> new TeamClient(clientOptions));
         this.introspectClient = Suppliers.memoize(() -> new IntrospectClient(clientOptions));
         this.labTestsClient = Suppliers.memoize(() -> new LabTestsClient(clientOptions));
         this.testkitClient = Suppliers.memoize(() -> new TestkitClient(clientOptions));
@@ -102,20 +102,16 @@ public class Vital {
         this.aggregateClient = Suppliers.memoize(() -> new AggregateClient(clientOptions));
     }
 
+    public ProvidersClient providers() {
+        return this.providersClient.get();
+    }
+
     public UserClient user() {
         return this.userClient.get();
     }
 
     public LinkClient link() {
         return this.linkClient.get();
-    }
-
-    public ProvidersClient providers() {
-        return this.providersClient.get();
-    }
-
-    public TeamClient team() {
-        return this.teamClient.get();
     }
 
     public ElectrocardiogramClient electrocardiogram() {
@@ -160,6 +156,10 @@ public class Vital {
 
     public VitalsClient vitals() {
         return this.vitalsClient.get();
+    }
+
+    public TeamClient team() {
+        return this.teamClient.get();
     }
 
     public IntrospectClient introspect() {
--- src/main/java/com/vital/api/core/ClientOptions.java
+++ src/main/java/com/vital/api/core/ClientOptions.java
@@ -30,7 +30,7 @@ public final class ClientOptions {
             {
                 put("X-Fern-Language", "JAVA");
                 put("X-Fern-SDK-Name", "com.vital.fern:api-sdk");
-                put("X-Fern-SDK-Version", "1.2.423");
+                put("X-Fern-SDK-Version", "1.2.424");
             }
         });
         this.headerSuppliers = headerSuppliers;
